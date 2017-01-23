40ba5320a82bc295f939140a5b8d9394cf5f306b
Udo <writeudo@gmail.com>
2017-01-23 09:35:53 +0100

    addes store function

---
+++ public/app/store/dev.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import { createStore, compose } from 'redux';
+import { persistState } from 'redux-devtools';
+import rootReducer from '../reducers';
+import DevTools from '../containers/devtools.jsx';
+
+const createStoreWithMiddleware = compose(
+  DevTools.instrument(),
+  persistState(getDegubSessionKey())
+)(createStore);
+
+function getDegubSessionKey() {
+  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
+  return (matches && matches.length > 0) ? matches[1] : null;
+};
+
+export default function configureStore(initialState) {
+  const store = createStoreWithMiddleware(rootReducer, initialState);
+
+  if (module.hot) {
+    // Enable webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRouter = require('../reducers/index').default;
+      store.replaceReducer('nextRouter');
+    });
+
+  }
+
+  return store;
+};
---
+++ public/app/store/prod.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+  return createStore(rootReducer, initialState);
+};
