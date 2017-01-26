c4e8ada3505012fdbac96e96fa014144d697347d
Udo <writeudo@gmail.com>
2017-01-26 09:45:30 +0100

    move webpack constants to own file

--- public/webpack/constants.js
+++ public/webpack/constants.js
@@ -2,6 +2,5 @@ const path = require('path');
 
 module.exports = {
   app: path.resolve(__dirname, '..', 'app/index.jsx'),
-  build: path.resolve(__dirname, '..', 'public/build'),
-  style: path.resolve(__dirname, '..', 'scss/main.scss'),
+  build: path.resolve(__dirname, '..', 'public/build')
 };
