dbd67523f52444cdf65b10493ac09edfd93bd14c
Udo <writeudo@gmail.com>
2017-02-06 07:33:25 +0100

    implement word counter in tweet form

--- public/app/containers/form.jsx
+++ public/app/containers/form.jsx
@@ -7,13 +7,23 @@ class Form extends React.Component {
 
   constructor() {
     super();
+    this.state = { charCount: 140 };
+    this._handleCount = this._handleCount.bind(this);
+  }
+
+  _handleCount(e) {
+    let charsLength = e.target.value.length;
+    console.log(charsLength);
+    if (charsLength < 141) {
+      this.setState({ charCount: 140 - charsLength });
+    }
   }
 
   render() {
     return (
       <form className="form">
-      	<textarea></textarea>
-      	<span className="counter"></span>
+      	<textarea onKeyUp={this._handleCount} maxLength="140"></textarea>
+      	<span className="counter">{this.state.charCount}</span>
       </form>
     )
   }
