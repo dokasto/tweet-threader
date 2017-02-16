ccc591b7893e8cd5d0fea895a171a06005537c85
Udo (Office) <writeudo@gmail.com>
2017-02-16 17:19:24 +0100

    updated reducers

--- public/app/reducers/index.js
+++ public/app/reducers/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 import { combineReducers } from 'redux';
-import tweetReducer from './tweet';
 import authReducer from './auth';
+import forms from './forms';
 
 export default combineReducers({
-  tweetReducer,
+	forms,
   authReducer
 });
