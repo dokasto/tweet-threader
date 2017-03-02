ec9bbe3a5be593c664807696246acdf58317baaa
Udo (Office) <writeudo@gmail.com>
2017-03-02 13:27:25 +0100

    remove unused reducer

--- public/app/reducers/auth.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export default function authReducer(state, action) {
-  var newState = {};
-  return newState;
-};
--- public/app/reducers/index.js
+++ public/app/reducers/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 import { combineReducers } from 'redux';
-import authReducer from './auth';
 import forms from './forms';
 
 export default combineReducers({
-	forms,
-  authReducer
+  forms
 });
