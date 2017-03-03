d66165eeb3cdff470aa7d5f21699c45da4f57dd8
Udo (Office) <writeudo@gmail.com>
2017-03-03 18:02:23 +0100

    redirect to host

--- public/app/components/tweet_page.jsx
+++ public/app/components/tweet_page.jsx
@@ -27,8 +27,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   let string = document.querySelector('#root').getAttribute('data-user');
 
   if (string === 'undefined') {
-    console.log(process.env.HOST_NAME);
-    window.location = process.env.HOST_NAME + '/';
+    window.location = window.location.hostname;
   }
 
   let buffer = new Buffer(string, 'base64');
