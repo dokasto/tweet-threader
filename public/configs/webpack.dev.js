31cefc259d5de866a5add45488c1ed041984c3d2
Udo (Office) <writeudo@gmail.com>
2017-03-06 13:46:36 +0100

    added postcss to webpack build

--- public/configs/webpack.dev.js
+++ public/configs/webpack.dev.js
@@ -3,6 +3,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const PATHS = require('./constants');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
 
@@ -47,12 +48,18 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
   ],
 
+  postcss: function() {
+    return [autoprefixer({
+      browsers: ['last 3 versions']
+    })];
+  },
+
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /(node_modules|api)/, loaders: ['babel-loader'] },
       { test: /\.json$/, loader: 'json' },
       { test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css'] },
-      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.scss$/, loader: 'style!css!postcss!sass' },
       { test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=100000' },
       { test: /.(woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url?limit=100000' }
     ],
