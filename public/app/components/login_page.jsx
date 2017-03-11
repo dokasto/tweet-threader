8652ef606c41657afd613df6396f3325a053c4f5
Udo <writeudo@gmail.com>
2017-03-11 12:47:05 +0100

    update home page copy

--- public/app/components/login_page.jsx
+++ public/app/components/login_page.jsx
@@ -11,14 +11,19 @@ class LoginPage extends React.Component {
 					<h1>Threader</h1>
 					<p>This tool allows you create threads on Twitter. All you need do is compose chunk of tweets and they will be 
 					threaded.</p>
-		    	<a href="/auth/twitter" className="button">Simply login with Twitter to start</a>
+		    	<a href="/auth/twitter" className="button login-btn">Simply login with Twitter to start</a>
 		    	<section>
 		    		<ul>
 		    			<li>- No information is stored.</li>
-		    			<li>- No ads.</li>
+		    			<li>- No ads, no tracking.</li>
 		    			<li>- No signup required.</li>
 		    			<li>- No bullshit.</li>
 		    		</ul>
+
+		    		<label className="copy">
+		    		This project was created using NodeJS(back-end), React-Redux(frontend) and deployed via Heroku.
+		    		It was developed and designed by <a href="http://udo.com.ng">Udo Nkwocha</a>
+		    		</label>
 		    	</section>
 		  	</main>
 		  </div>
