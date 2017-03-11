8816151bde70f67cbf51801ddf23a484f33cf31f
Udo <writeudo@gmail.com>
2017-03-11 12:38:00 +0100

    edit copy on home page

--- public/app/components/login_page.jsx
+++ public/app/components/login_page.jsx
@@ -9,8 +9,17 @@ class LoginPage extends React.Component {
       <div className="login-page">
 		  	<main>
 					<h1>Threader</h1>
-					<p>A better way to create threads on Twitter.</p>
-		    	<a href="/auth/twitter" className="button">Login to start</a>
+					<p>This tool allows you create threads on Twitter. All you need do is compose chunk of tweets and they will be 
+					threaded.</p>
+		    	<a href="/auth/twitter" className="button">Simply login with Twitter to start</a>
+		    	<section>
+		    		<ul>
+		    			<li>- No information is stored.</li>
+		    			<li>- No ads.</li>
+		    			<li>- No signup required.</li>
+		    			<li>- No bullshit.</li>
+		    		</ul>
+		    	</section>
 		  	</main>
 		  </div>
     );
