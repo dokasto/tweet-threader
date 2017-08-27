3804b91c45a411fc98b2c7ff945d3ca010290b62
Udo <writeudo@gmail.com>
2017-08-27 21:38:06 +0200

    fix tweet page

--- public/app/components/tweet.jsx
+++ public/app/components/tweet.jsx
@@ -1,67 +1,60 @@
 import React, { PropTypes } from 'react';
 import Form from '../containers/form.jsx';
 
-const Tweet = ({ onAddForm, onRemove, onChange, postTweets, forms, person, notifications }) => {
-	const _onClick = e => {
-		e.preventDefault();
-		postTweets(forms);
-	};
-
-	return (
-		<div className="tweet-page">
-			<div className={notifications.type === 'ongoing' ? 'disabled' : ''}>
-				<section className="container">
-					<header>
-						<aside>
-							<picture className="left">
-								<img alt="profile" src={person.photos[0].value} />
-							</picture>
-							<div className="right">
-								<span className="fullname">
-									{person.displayName}
-								</span>
-								<span className="username">
+const Tweet = ({ onAddForm, onRemove, onChange, postTweets, forms, person, notifications }) => (
+	<div className="tweet-page">
+		<div className={notifications.type === 'ongoing' ? 'disabled' : ''}>
+			<section className="container">
+				<header>
+					<aside>
+						<picture className="left">
+							<img alt="profile" src={person.photos[0].value} />
+						</picture>
+						<div className="right">
+							<span className="fullname">
+								{person.displayName}
+							</span>
+							<span className="username">
 									@{person.username}
-								</span>
-							</div>
-						</aside>
-						<button className="button post-btn" disabled={forms.length < 1} onClick={_onClick} type="submit">
-							Post
-						</button>
-					</header>
-					{notifications
-						? <div className="notifications">
-							<span className={notifications.type}>
-								{notifications.message}
 							</span>
 						</div>
-						: null}
-					<div className="tweet-forms">
-						<div className={forms.length <= 1 ? 'hide-remove-btn' : ''}>
-							{forms.map((each, index) =>
-								<Form
-									key={each.id}
-									id={each.id}
-									index={index}
-									formCount={forms.length}
-									status={each.status}
-									text={each.text}
-									onChange={onChange}
-									onRemove={onRemove}
-								/>,
-							)}
-						</div>
-						<div className="form-control">
-							<button onClick={onAddForm} className="button thread-btn">
-								+ Thread
-							</button>
-						</div>
+					</aside>
+					<button className="button post-btn" disabled={forms.length < 1} onClick={() => postTweets(forms)} type="submit">
+							Post
+					</button>
+				</header>
+				{notifications
+					? <div className="notifications">
+						<span className={notifications.type}>
+							{notifications.message}
+						</span>
 					</div>
-				</section>
-			</div>
+					: null}
+				<div className="tweet-forms">
+					<div className={forms.length <= 1 ? 'hide-remove-btn' : ''}>
+						{forms.map((each, index) =>
+							<Form
+								key={each.id}
+								id={each.id}
+								index={index}
+								formCount={forms.length}
+								status={each.status}
+								text={each.text}
+								onChange={onChange}
+								onRemove={onRemove}
+							/>,
+						)}
+					</div>
+					<div className="form-control">
+						<button onClick={onAddForm} className="button thread-btn">
+								+ Thread
+						</button>
+					</div>
+				</div>
+			</section>
 		</div>
-	);
-};
+	</div>
+);
 
 Tweet.propTypes = {
 	postTweets: PropTypes.func.isRequired,
