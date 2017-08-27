55449586c7e4c5703e78d7d0f4a0255bf61436c9
Udo <writeudo@gmail.com>
2017-08-27 21:34:38 +0200

    fix login page

--- public/app/components/login_page.jsx
+++ public/app/components/login_page.jsx
@@ -1,36 +1,28 @@
-'use strict';
-
 import React from 'react';
 
-class LoginPage extends React.Component {
-
-  render() {
-    return (
-      <div className="login-page">
-		  	<main>
-					<h1>TweetThreader</h1>
-					<p>This tool allows you create threads on Twitter. All you need do is compose chunk of tweets and they will be 
-					threaded.</p>
-		    	<a href="/auth/twitter" className="button login-btn">Simply login with Twitter to start</a>
-		    	<section>
-		    		<ul>
-		    			<li>- No information is stored.</li>
-		    			<li>- No ads, no tracking.</li>
-		    			<li>- No signup required.</li>
-		    			<li>- No bullshit.</li>
-		    		</ul>
-
-		    		<label className="copy">
-		    		<strong>Notes for Nerds</strong> <br />
-		    		This project was created using NodeJS(back-end), React-Redux(frontend) and deployed via Heroku.
-		    		<br />
-		    		It was developed and designed by <a href="http://udo.com.ng">Udo Nkwocha</a>
-		    		</label>
-		    	</section>
-		  	</main>
-		  </div>
-    );
-  }
-}
+const LoginPage = () =>
+	<div className="login-page">
+		<main>
+			<h1>TweetThreader</h1>
+			<p>This tool allows you create threads on Twitter. All you need do is compose chunk of tweets and they will be threaded.</p>
+			<a href="/auth/twitter" className="button login-btn">
+				Simply login with Twitter to start
+			</a>
+			<section>
+				<ul>
+					<li>- No information is stored.</li>
+					<li>- No ads, no tracking.</li>
+					<li>- No signup required.</li>
+					<li>- No bullshit.</li>
+				</ul>
+				<span className="copy">
+					<strong>Notes for Nerds</strong> <br />
+					This project was created using NodeJS(back-end), React-Redux(frontend) and deployed via Heroku.
+					<br />
+					It was developed and designed by <a href="http://udo.com.ng">Udo Nkwocha</a>
+				</span>
+			</section>
+		</main>
+	</div>;
 
 export default LoginPage;
