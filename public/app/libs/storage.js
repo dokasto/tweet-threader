9d4b26e1e1f587c2333eb1189b67877004b59699
Udo <writeudo@gmail.com>
2017-08-29 23:55:01 +0200

    format using eslint

--- public/app/libs/storage.js
+++ public/app/libs/storage.js
@@ -1,12 +1,12 @@
 export default {
-  get: function(key) {
-    try {
-      return JSON.parse(localStorage.getItem(key));
-    } catch (e) {
-      return null;
-    }
-  },
-  set: function(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-  }
+	get(key) {
+		try {
+			return JSON.parse(localStorage.getItem(key));
+		} catch (e) {
+			return null;
+		}
+	},
+	set(key, data) {
+		localStorage.setItem(key, JSON.stringify(data));
+	}
 };
