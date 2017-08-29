768dec93a96f6251b81c80fa1662636b40c5019c
Udo <writeudo@gmail.com>
2017-08-29 23:56:10 +0200

    lint

--- public/app/reducers/index.js
+++ public/app/reducers/index.js
@@ -1,10 +1,5 @@
-'use strict';
-
 import { combineReducers } from 'redux';
 import forms from './forms';
 import notifications from './notifications';
 
-export default combineReducers({
-  forms,
-  notifications
-});
+export default combineReducers({ forms, notifications });
